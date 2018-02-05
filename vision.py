d05cb14598fb2406dfa335f146c40b50ebf29006
amintz <andregmintz@gmail.com>
2018-02-05 14:51:34 +0000

    implemented image-label network files output. error handling improvements.

--- vision.py
+++ vision.py
@@ -62,6 +62,10 @@ forceBase64 = f.yn(settings['SourceImagesLocation']['ForceBase64'])
 saveImageCopy = f.yn(settings['SourceImagesLocation']['SaveImageCopy'])
 inputImageFolder = settings['SourceImagesLocation']['InputImageFolder']
 
+makeNetwork = f.yn(settings['ImageLabelNetworkOutput']['MakeNetwork'])
+includeLink = f.yn(settings['ImageLabelNetworkOutput']['IncludeLink'])
+linkColumn = settings['ImageLabelNetworkOutput']['InputFileLinkColumn']
+
 print("HANDLING OF IMAGE SOURCE\n")
 if imagesRemote:
     print("\tOnline images")
@@ -113,6 +117,13 @@ else:
     print(inputHeader)
     sys.exit('\n**ERROR**\nImage column could not be found in input file. Please check the configuration file.\n')
 
+if makeNetwork:
+    if linkColumn in inputHeader:
+        linkColumnIdx = inputHeader.index(linkColumn)
+    else:
+        print(inputHeader)
+        sys.exit('\n**ERROR**\nLink column could not be found in input file. Please check the configuration file\n')
+
 # Workaround for Facebook URLs messing with filename extensions
 if ("created_time_unix" in inputHeader) or  ("post_published_unix" in inputHeader):
     isFacebook = True
@@ -134,7 +145,7 @@ print("\tProcessing limit\n\t\t" + str(procLimit))
 print("\tImage column header\n\t\t" + imagesColumn + "(" + str(imagesColumnIdx) + ")")
 print("\n-------------------------")
 # ------------------------------------------
-# Create output file
+# Create output files
 # ------------------------------------------
 
 outputFileName = "processed_" + inputFileName
@@ -151,6 +162,29 @@ outputCSV = csv.writer(outputFile, csvDialect)
 outputHeader = inputHeader + ['original_filename', 'image_id', 'file_ext', 'copy_filename', 'gv_ss_adult','gv_ss_spoof','gv_ss_medical','gv_ss_violence','gv_labels', 'gv_text', 'gv_text_lang', 'gv_web_entities', 'gv_web_full_matching_images', 'gv_web_partial_matching_images', 'gv_web_pages_with_matching_images', 'gv_web_visually_similar_images', 'gv_num_faces', 'gv_face_joy', 'gv_face_sorrow', 'gv_face_anger', 'gv_face_surprise']
 
 outputCSV.writerow(outputHeader)
+
+if makeNetwork:
+    nodesfilename = "nodes_" + inputFileName.split(".")[0] + ".csv"
+    nodesfilepath = outputsFolder + nodesfilename
+    nodesheader = ['id', 'type', 'label', 'gv_label', 'filename', 'link']
+    try:
+        nodesfile = open(nodesfilepath,'w', newline='', encoding='utf8')
+    except Exception:
+        sys.exit('\n**ERROR**\nNodes file could not be created. Please check the configuration file\n')
+    nodesCSV = csv.writer(nodesfile,csvDialect, delimiter=",")
+    nodesCSV.writerow(nodesheader)
+
+    edgesfilename = "edges_" + inputFileName.split(".")[0] + ".csv"
+    edgesfilepath = outputsFolder + edgesfilename
+    edgesheader = ['source', 'target', 'weight']
+    try:
+        edgesfile = open(edgesfilepath, 'w', newline='', encoding='utf8')
+    except Exception:
+        sys.exit('\n**ERROR**\Edges file could not be created. Please check the configuration file\n')
+    edgesCSV = csv.writer(edgesfile,csvDialect, delimiter=",")
+    edgesCSV.writerow(edgesheader)
+    foundlabels = []
+
 
 # ------------------------------------------
 # Process images
@@ -183,6 +217,7 @@ for i in range(procLimit):
     # Create hash for image url
     hashObj = hashlib.sha1(imagePath.encode('utf-8'))
     imageHash = hashObj.hexdigest()
+    print("\tImage ID: %s" % (imageHash))
 
     # Make image copy
     copyFilename = imageHash + extension
@@ -194,11 +229,27 @@ for i in range(procLimit):
             try:
                 image = requests.get(imagePath, allow_redirects=True)
             except Exception:
-                sys.exit('\n**ERROR**\nCould not retrieve remote image. Check data, internet connection, configuration.\n')
+                print('\n\t**ERROR**\n\tCould not retrieve remote image. Check data, internet connection, configuration.\n')
+                outputCSV.writerow(outputRow)
+                continue
             open(copyFilePath,"wb").write(image.content)
             print("done")
         else:
             print("\tCopy already existed")
+
+    if makeNetwork:
+        link = inputRow[linkColumnIdx]
+        noderow = []
+        noderow.append(imageHash)
+        noderow.append('image')
+        if includeLink:
+            noderow.append('image,'+ copyFilename + ',' + link)
+        else:
+            noderow.append(copyFilename)
+        noderow.append('')
+        noderow.append(copyFilename)
+        noderow.append(link)
+        nodesCSV.writerow(noderow)
 
     # Process image
 
@@ -210,10 +261,12 @@ for i in range(procLimit):
             # If images are remote but are to be processed through local base64 coding and copies have been made, use copies for network traffic efficiency.
             responseData = gapi.processImage(copyFilePath, remote=False)
             if not responseData:
+                outputCSV.writerow(outputRow)
                 continue
         else:
             responseData = gapi.processImage(imagePath)
             if not responseData:
+                outputCSV.writerow(outputRow)
                 continue
         with open(responseFile, 'w', encoding='utf8') as outFile:
             json.dump(responseData, outFile, indent=4, sort_keys=True)
@@ -225,13 +278,19 @@ for i in range(procLimit):
         responseData = json.load(open(responseFile, encoding='utf8'))
 
     # Parse API response
+    try:
+        response = responseData['responses'][0]
+    except Exception:
+        print('\t*ATTENTION* Vision API returned an error for this image. Check JSON file for details.\n\tMoving on to next image.\n')
+        outputCSV.writerow(outputRow)
+        continue
 
     # Safe Search
-    if 'safeSearchAnnotation' in responseData['responses'][0]:
-        gv_ss_adult = responseData['responses'][0]['safeSearchAnnotation']['adult']
-        gv_ss_spoof = responseData['responses'][0]['safeSearchAnnotation']['spoof']
-        gv_ss_medical = responseData['responses'][0]['safeSearchAnnotation']['medical']
-        gv_ss_violence = responseData['responses'][0]['safeSearchAnnotation']['violence']
+    if 'safeSearchAnnotation' in response:
+        gv_ss_adult = response['safeSearchAnnotation']['adult']
+        gv_ss_spoof = response['safeSearchAnnotation']['spoof']
+        gv_ss_medical = response['safeSearchAnnotation']['medical']
+        gv_ss_violence = response['safeSearchAnnotation']['violence']
     else:
         gv_ss_adult = "NONE"
         gv_ss_spoof = "NONE"
@@ -239,56 +298,76 @@ for i in range(procLimit):
         gv_ss_violence = "NONE"
 
     # Labels
-    if 'labelAnnotations' in responseData['responses'][0]:
+    if 'labelAnnotations' in response:
         gv_labels = []
-        for label in responseData['responses'][0]['labelAnnotations']:
+        for label in response['labelAnnotations']:
+            if not label['mid'] in foundlabels:
+                foundlabels.append(label['mid'])
+                if makeNetwork:
+                    noderow = []
+                    noderow.append(label['mid'])
+                    noderow.append('gv_label')
+                    if includeLink:
+                        noderow.append('gv_label,,')
+                    else:
+                        noderow.append('')
+                    noderow.append(label['description'])
+                    noderow.append('')
+                    noderow.append('')
+                    nodesCSV.writerow(noderow)
+            if makeNetwork:
+                edgerow =[]
+                edgerow.append(imageHash)
+                edgerow.append(label['mid'])
+                edgerow.append(label['score'])
+                edgesCSV.writerow(edgerow)
             gv_labels.append(label['description'] + "(" + str(label['score']) + ")")
         gv_labels = ",".join(gv_labels)
     else:
         gv_labels = "NONE"
 
     # Text
-    if 'textAnnotations' in responseData['responses'][0]:
-        gv_text = responseData['responses'][0]['textAnnotations'][0]['description']
+    if 'textAnnotations' in response:
+        gv_text = response['textAnnotations'][0]['description']
         gv_text = re.sub("[\n\t\r]", " ", gv_text)
-        gv_text_lang = responseData['responses'][0]['textAnnotations'][0]['locale']
+        gv_text_lang = response['textAnnotations'][0]['locale']
     else:
         gv_text = "NONE"
         gv_text_lang = "NONE"
 
     # Web Detection
-    if 'webDetection' in responseData['responses'][0]:
-        if 'fullMatchingImages' in responseData['responses'][0]['webDetection']:
+    if 'webDetection' in response:
+        if 'fullMatchingImages' in response['webDetection']:
             gv_web_full_matching_images = []
-            for url in responseData['responses'][0]['webDetection']['fullMatchingImages']:
+            for url in response['webDetection']['fullMatchingImages']:
                 gv_web_full_matching_images.append(url['url'].replace(",", "%2C"))
             gv_web_full_matching_images = ",".join(gv_web_full_matching_images)
         else:
             gv_web_full_matching_images = "NONE"
-        if 'pagesWithMatchingImages' in responseData['responses'][0]['webDetection']:
+        if 'pagesWithMatchingImages' in response['webDetection']:
             gv_web_pages_with_matching_images = []
-            for url in responseData['responses'][0]['webDetection']['pagesWithMatchingImages']:
+            for url in response['webDetection']['pagesWithMatchingImages']:
                 gv_web_pages_with_matching_images.append(url['url'].replace(",", "%2C"))
             gv_web_pages_with_matching_images = ",".join(gv_web_pages_with_matching_images)
         else:
             gv_web_pages_with_matching_images = "NONE"
-        if 'partialMatchingImages' in responseData['responses'][0]['webDetection']:
+        if 'partialMatchingImages' in response['webDetection']:
             gv_web_partial_matching_images = []
-            for url in responseData['responses'][0]['webDetection']['partialMatchingImages']:
+            for url in response['webDetection']['partialMatchingImages']:
                 gv_web_partial_matching_images.append(url['url'].replace(",", "%2C"))
             gv_web_partial_matching_images = ",".join(gv_web_partial_matching_images)
         else:
             gv_web_partial_matching_images = "NONE"
-        if 'visuallySimilarImages' in responseData['responses'][0]['webDetection']:
+        if 'visuallySimilarImages' in response['webDetection']:
             gv_web_visually_similar_images = []
-            for url in responseData['responses'][0]['webDetection']['visuallySimilarImages']:
+            for url in response['webDetection']['visuallySimilarImages']:
                 gv_web_visually_similar_images.append(url['url'].replace(",", "%2C"))
             gv_web_visually_similar_images = ",".join(gv_web_visually_similar_images)
         else:
             gv_web_visually_similar_images = "NONE"
-        if 'webEntities' in responseData['responses'][0]['webDetection']:
+        if 'webEntities' in response['webDetection']:
             gv_web_entities = []
-            for entity in responseData['responses'][0]['webDetection']['webEntities']:
+            for entity in response['webDetection']['webEntities']:
                 if 'description' in entity:
                     description = entity['description']
                 else:
@@ -305,13 +384,13 @@ for i in range(procLimit):
         gv_web_entities = "NONE"
 
     # Face
-    if 'faceAnnotations' in responseData['responses'][0]:
+    if 'faceAnnotations' in response:
         gv_num_faces = 0;
         gv_face_joy = 'VERY_UNLIKELY'
         gv_face_sorrow = 'VERY_UNLIKELY'
         gv_face_anger = 'VERY_UNLIKELY'
         gv_face_surprise = 'VERY_UNLIKELY'
-        for face in responseData['responses'][0]['faceAnnotations']:
+        for face in response['faceAnnotations']:
             gv_face_joy = f.likelihoodCompare(gv_face_joy, face['joyLikelihood'])
             gv_face_sorrow = f.likelihoodCompare(gv_face_sorrow, face['sorrowLikelihood'])
             gv_face_anger = f.likelihoodCompare(gv_face_anger, face['angerLikelihood'])
